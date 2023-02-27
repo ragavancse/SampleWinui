8a67296f428e8fb8c5bbc296cf9b3369b7bb4e67
ragavan <Admin@DESKTOP-6VND80J>
2023-02-27 17:38:40 +0530

    ApplicationInstance and PMMSSetup pages

---
+++ SampleWinui/View/ApplicationInstances.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using SampleWinui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SampleWinui.View
+{
+    public sealed partial class ApplicationInstances : UserControl
+    {
+        public ApplicationInstances()
+        {
+            this.InitializeComponent();
+            this.DataContext = new ApplicationViewModel();
+        }
+    }
+}
