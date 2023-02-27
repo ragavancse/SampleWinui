287864602dcde981f6bbbba1ca4ec9fbd546d551
ragavancse <ragavancse@gmail.com>
2023-02-27 18:43:14 +0530

    Childcontrols changes

--- SampleWinui/ChildWindowControls/ChildWindow.xaml.cs
+++ SampleWinui/ChildWindowControls/ChildWindow.xaml.cs
@@ -1,42 +1,38 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using SampleWinui.ChildWindowControls;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI;
-using Color = Windows.UI.Color;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SampleWinui.View
+namespace SampleWinui.ChildWindowControls
 {
-    public sealed partial class Setup : UserControl
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ChildWindow : Window
     {
-        public Setup()
+        public ChildWindow()
         {
             this.InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void SetContent(UserControl userControl)
         {
-            var window = new ChildWindow();
-            window.SetContent(new Backup());
-            window.Activate();
+            content.Content = userControl;
         }
     }
 }
