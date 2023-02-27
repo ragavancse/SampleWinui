f064b5e95d5815f20d1a9bef1efd03422134c525
ragavancse <ragavancse@gmail.com>
2023-02-27 16:57:27 +0530

    Added File View

---
+++ SampleWinui/Model/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWinui.Model
+{
+    public class FileModel
+    {
+        public string Icon { get; set; }
+        public string HeaderText { get; set; }
+        public string FooterText { get; set; }
+    }
+}
---
+++ SampleWinui/View/FileView.xaml.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using SampleWinui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SampleWinui.View
+{
+    public sealed partial class FileView : UserControl
+    {
+        public FileView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void nvSample8_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavigationViewItem selectedItem = args.SelectedItem as NavigationViewItem;
+            switch (selectedItem.Tag)
+            {
+                case "SystemUtilities":
+                    ContentFrame.Navigate(typeof(SystemUtilities));
+                    break;
+                case "Setup":
+                    ContentFrame.Navigate(typeof(Setup));
+                    break;
+              
+            }
+        }
+    }
+}
