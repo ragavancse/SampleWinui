825779c5c08b010fec526c8f1d55cbafef7865c6
ragavancse@gmail.com <ragavancse@gmail.com>
2023-02-28 17:27:03 +0530

    Make the Paging control as common one

--- SampleWinui/CommonControls/PaginationControl.xaml.cs
+++ SampleWinui/CommonControls/PaginationControl.xaml.cs
@@ -1,230 +1,229 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using SampleWinui.Common;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows.Input;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace SampleWinui.CommonControls
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class PaginationControl : UserControl
+    public sealed partial class PaginationControl : UserControl, INotifyPropertyChanged
     {
         public PaginationControl()
         {
             this.InitializeComponent();
         }
 
+        #region Properties
+
+        public int CurrentPageIndex = 0;
+        public int ItemPerPage = 10;
+
+        private List<string> _PageSizeList = new() { "10", "20", "50", "100", "250" };
+        public List<string> PageSizeList
+        {
+            get
+            {
+                return _PageSizeList;
+            }
+            set
+            {
+                _PageSizeList = value;
+                RaisePropertyChanged(nameof(PageSizeList));
+            }
+        }
+
+        private string _PageSize = "10";
+
+        public string PageSize
+        {
+            get
+            {
+                return _PageSize;
+            }
+            set
+            {
+                _PageSize = value;
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    bool isInt = int.TryParse(value, out int valueInt);
+                    ItemPerPage = isInt ? valueInt : ItemPerPage;
+                    UpdateTotalPageCount();
+                }
+            }
+        }
+
+        private int _CurrentPage;
+
+        public int CurrentPage
+        {
+            get { return _CurrentPage; }
+            set
+            {
+                _CurrentPage = value;
+                RaisePropertyChanged(nameof(CurrentPage));
+            }
+        }
+
+        private int _TotalPage;
+
+        public int TotalPage
+        {
+            get { return _TotalPage; }
+            set
+            {
+                _TotalPage = value;
+                RaisePropertyChanged(nameof(TotalPage));
+            }
+        }
+
+        private int _CurrentPageStart;
+
+        public int CurrentPageStart
+        {
+            get { return _CurrentPageStart; }
+            set
+            {
+                _CurrentPageStart = value;
+                RaisePropertyChanged(nameof(CurrentPageStart));
+            }
+        }
+
+        private int _CurrentPageEnd;
+
+        public int CurrentPageEnd
+        {
+            get { return _CurrentPageEnd; }
+            set
+            {
+                _CurrentPageEnd = value;
+                RaisePropertyChanged(nameof(CurrentPageEnd));
+            }
+        }
+
+        private int _TotalItems;
+
+        public int TotalItems
+        {
+            get { return _TotalItems; }
+            set
+            {
+                _TotalItems = value;
+                RaisePropertyChanged(nameof(TotalItems));
+            }
+        }
+
+        #endregion
+
         #region Event Handlers
 
         private void btnFirst_Click(object sender, RoutedEventArgs e)
         {
-            FirstPageCommand?.Execute(null);
+            if (CurrentPageIndex != 0)
+                CurrentPageIndex = 0;
+
+            RunPageSizeUpdate();
         }
 
         private void btnPrev_Click(object sender, RoutedEventArgs e)
         {
-            PrevPageCommand?.Execute(null);
+            if (CurrentPageIndex > 0)
+                CurrentPageIndex--;
+
+            ShowCurrentPageIndex();
         }
 
         private void btnNext_Click(object sender, RoutedEventArgs e)
         {
-            NextPageCommand?.Execute(null);
+            if (CurrentPageIndex < TotalPage - 1)
+                CurrentPageIndex++;
+
+            ShowCurrentPageIndex();
         }
 
         private void btnLast_Click(object sender, RoutedEventArgs e)
         {
-            LastPageCommand?.Execute(null);
+            if (CurrentPageIndex != TotalPage - 1)
+                CurrentPageIndex = TotalPage - 1;
+
+            ShowCurrentPageIndex();
+        }
+
+        private void RunPageSizeUpdate()
+        {
+            if (Source.Count() > 0)
+                UpdateTotalPageCount();
+        }
+
+        private void ShowCurrentPageIndex()
+        {
+            if (Source.Count() > 0)
+            {
+                FilteredSource = new ObservableCollection<object>(Source.Skip(CurrentPageIndex * ItemPerPage).Take(ItemPerPage));
+                CurrentPage = CurrentPageIndex + 1;
+                CurrentPageStart = (CurrentPageIndex * ItemPerPage) + 1;
+                CurrentPageEnd = (CurrentPageIndex + 1) * ItemPerPage > TotalItems ? TotalItems : (CurrentPageIndex + 1) * ItemPerPage;
+            }
+        }
+
+        public void UpdateTotalPageCount()
+        {
+            CurrentPageIndex = 0;
+            TotalPage = Source.Count() % ItemPerPage == 0 ? Source.Count() / ItemPerPage : Source.Count() / ItemPerPage + 1;
+            ShowCurrentPageIndex();
         }
 
         #endregion Event Handlers
 
         #region Dependency Properties
 
-        public int CurrentPage
+        public IEnumerable<object> Source
         {
-            get { return (int)GetValue(CurrentPageProperty); }
-            set { SetValue(CurrentPageProperty, value); }
+            get { return (IEnumerable<object>)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for CurrentPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CurrentPageProperty =
-            DependencyProperty.Register("CurrentPage", typeof(int), typeof(PaginationControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register("Source",
+                                typeof(IEnumerable<>),
+                                typeof(PaginationControl),
+                                new PropertyMetadata(null, new PropertyChangedCallback(SourceChanged)));
 
-        public string PageSize
+        private static void SourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            get { return (string)GetValue(PageSizeProperty); }
-            set { SetValue(PageSizeProperty, value); }
+            PaginationControl ctrl = d as PaginationControl;
+            if (ctrl != null)
+            {
+                ctrl.TotalItems = ctrl.Source.Count();
+                ctrl.UpdateTotalPageCount();
+            }
         }
 
-        // Using a DependencyProperty as the backing store for CurrentPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PageSizeProperty =
-            DependencyProperty.Register("PageSize", typeof(string), typeof(PaginationControl), new PropertyMetadata(string.Empty));
-
-        public List<string> PageSizeList
+        public IEnumerable<object> FilteredSource
         {
-            get { return (List<string>)GetValue(PageSizeListProperty); }
-            set { SetValue(PageSizeListProperty, value); }
+            get { return (IEnumerable<object>)GetValue(FilteredSourceProperty); }
+            set { SetValue(FilteredSourceProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for CurrentPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PageSizeListProperty =
-            DependencyProperty.Register("PageSizeList", typeof(List<int>), typeof(PaginationControl), new PropertyMetadata(null));
-
-        public int TotalPage
-        {
-            get { return (int)GetValue(TotalPageProperty); }
-            set { SetValue(TotalPageProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for TotalPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TotalPageProperty =
-            DependencyProperty.Register("TotalPage", typeof(int), typeof(PaginationControl), new PropertyMetadata(0));
-
-        public string CurrentPageStart
-        {
-            get { return (string)GetValue(CurrentPageStartProperty); }
-            set { SetValue(CurrentPageStartProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for TotalPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CurrentPageStartProperty =
-            DependencyProperty.Register("CurrentPageStart", typeof(string), typeof(PaginationControl), new PropertyMetadata(string.Empty));
-
-        public string CurrentPageEnd
-        {
-            get { return (string)GetValue(CurrentPageEndProperty); }
-            set { SetValue(CurrentPageEndProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for TotalPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CurrentPageEndProperty =
-            DependencyProperty.Register("CurrentPageEnd", typeof(string), typeof(PaginationControl), new PropertyMetadata(string.Empty));
-
-        public string TotalItems
-        {
-            get { return (string)GetValue(TotalItemsProperty); }
-            set { SetValue(TotalItemsProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for TotalPage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TotalItemsProperty =
-            DependencyProperty.Register("TotalItems", typeof(string), typeof(PaginationControl), new PropertyMetadata(string.Empty));
-
-        public ICommand FirstPageCommand
-        {
-            get { return (ICommand)GetValue(FirstPageCommandProperty); }
-            set { SetValue(FirstPageCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty FirstPageCommandProperty =
-        DependencyProperty.Register(
-        "FirstPageCommand",
-        typeof(ICommand),
-        typeof(PaginationControl), new PropertyMetadata(null));
-
-        public ICommand PrevPageCommand
-        {
-            get { return (ICommand)GetValue(PrevPageCommandProperty); }
-            set { SetValue(PrevPageCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty PrevPageCommandProperty =
-        DependencyProperty.Register(
-        "PrevPageCommand",
-        typeof(ICommand),
-        typeof(PaginationControl), new PropertyMetadata(null));
-
-        public ICommand NextPageCommand
-        {
-            get { return (ICommand)GetValue(NextPageCommandProperty); }
-            set { SetValue(NextPageCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty NextPageCommandProperty =
-        DependencyProperty.Register(
-        "NextPageCommand",
-        typeof(ICommand),
-        typeof(PaginationControl), new PropertyMetadata(null));
-
-        public ICommand LastPageCommand
-        {
-            get { return (ICommand)GetValue(LastPageCommandProperty); }
-            set { SetValue(LastPageCommandProperty, value); }
-        }
-
-        public static readonly DependencyProperty LastPageCommandProperty =
-        DependencyProperty.Register(
-        "LastPageCommand",
-        typeof(ICommand),
-        typeof(PaginationControl), new PropertyMetadata(null));
-
-        //public bool IsFirstPageButtonEnable
-        //{
-        //    get { return (bool)GetValue(IsFirstPageButtonEnableProperty); }
-        //    set { SetValue(IsFirstPageButtonEnableProperty, value); }
-        //}
-
-        //public static readonly DependencyProperty IsFirstPageButtonEnableProperty =
-        //DependencyProperty.Register(
-        //"IsFirstPageButtonEnable",
-        //typeof(bool),
-        //typeof(PaginationControl), new PropertyMetadata(true));
-
-        //public bool IsPrevPageButtonEnable
-        //{
-        //    get { return (bool)GetValue(IsPrevPageButtonEnableProperty); }
-        //    set { SetValue(IsPrevPageButtonEnableProperty, value); }
-        //}
-
-        //public static readonly DependencyProperty IsPrevPageButtonEnableProperty =
-        //DependencyProperty.Register(
-        //"IsPrevPageButtonEnable",
-        //typeof(bool),
-        //typeof(PaginationControl), new PropertyMetadata(true));
-
-        //public bool IsNextPageButtonEnable
-        //{
-        //    get { return (bool)GetValue(IsNextPageButtonEnableProperty); }
-        //    set { SetValue(IsNextPageButtonEnableProperty, value); }
-        //}
-
-        //public static readonly DependencyProperty IsNextPageButtonEnableProperty =
-        //DependencyProperty.Register(
-        //"IsNextPageButtonEnable",
-        //typeof(bool),
-        //typeof(PaginationControl), new PropertyMetadata(true));
-
-        //public bool IsLastPageButtonEnable
-        //{
-        //    get { return (bool)GetValue(IsLastPageButtonEnableProperty); }
-        //    set { SetValue(IsLastPageButtonEnableProperty, value); }
-        //}
-
-        //public static readonly DependencyProperty IsLastPageButtonEnableProperty =
-        //DependencyProperty.Register(
-        //"IsLastPageButtonEnable",
-        //typeof(bool),
-        //typeof(PaginationControl), new PropertyMetadata(true));
+        public static readonly DependencyProperty FilteredSourceProperty =
+        DependencyProperty.Register("FilteredSource", typeof(IEnumerable<>), typeof(PaginationControl), new PropertyMetadata(null));
 
         #endregion Dependency Properties
+
+        #region NotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
