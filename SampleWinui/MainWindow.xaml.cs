640640a8dede7b9352b4f7c37084470d0e7a775e
ragavancse <ragavancse@gmail.com>
2023-03-04 12:20:35 +0530

    App - limit the window size to a level where further reduction doesnt work so user is unable to shrink it less than FOR EXAMPLE - 300 or 400 width whatever

--- SampleWinui/MainWindow.xaml.cs
+++ SampleWinui/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -31,6 +32,97 @@ namespace SampleWinui
             this.InitializeComponent();
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
+            RegisterWindowMinMax(this);
+
+        }
+
+        private static WinProc newWndProc = null;
+        private static IntPtr oldWndProc = IntPtr.Zero;
+        private delegate IntPtr WinProc(IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        internal static extern int GetDpiForWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+        public static int MinWindowWidth { get; set; } = 750;
+        public static int MaxWindowWidth { get; set; } = 2500;
+        public static int MinWindowHeight { get; set; } = 600;
+        public static int MaxWindowHeight { get; set; } = 2500;
+
+
+        private static void RegisterWindowMinMax(Window window)
+        {
+            var hwnd = GetWindowHandleForCurrentWindow(window);
+
+            newWndProc = new WinProc(WndProc);
+            oldWndProc = SetWindowLongPtr(hwnd, WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
+        }
+
+        private static IntPtr GetWindowHandleForCurrentWindow(object target) =>
+            WinRT.Interop.WindowNative.GetWindowHandle(target);
+
+        private static IntPtr WndProc(IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (Msg)
+            {
+                case WindowMessage.WM_GETMINMAXINFO:
+                    var dpi = GetDpiForWindow(hWnd);
+                    var scalingFactor = (float)dpi / 96;
+
+                    var minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                    minMaxInfo.ptMinTrackSize.x = (int)(MinWindowWidth * scalingFactor);
+                    minMaxInfo.ptMaxTrackSize.x = (int)(MaxWindowWidth * scalingFactor);
+                    minMaxInfo.ptMinTrackSize.y = (int)(MinWindowHeight * scalingFactor);
+                    minMaxInfo.ptMaxTrackSize.y = (int)(MaxWindowHeight * scalingFactor);
+
+                    Marshal.StructureToPtr(minMaxInfo, lParam, true);
+                    break;
+
+            }
+            return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
+        }
+
+        private static IntPtr SetWindowLongPtr(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc)
+        {
+            if (IntPtr.Size == 8)
+                return SetWindowLongPtr64(hWnd, nIndex, newProc);
+            else
+                return new IntPtr(SetWindowLong32(hWnd, nIndex, newProc));
+        }
+
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+
+        [Flags]
+        private enum WindowLongIndexFlags : int
+        {
+            GWL_WNDPROC = -4,
+        }
+
+        private enum WindowMessage : int
+        {
+            WM_GETMINMAXINFO = 0x0024,
         }
 
         private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -38,7 +130,7 @@ namespace SampleWinui
             if (sender is Grid grid && grid.ActualWidth < 1400)
             {
                 Grid.SetColumn(rightPanel, 0);
-                rightPanel.Margin= new Thickness(0,50, 0, 0);
+                rightPanel.Margin = new Thickness(0, 50, 0, 0);
                 (pivot.SelectedItem as PivotItem).Margin = new Thickness(0, 35, 0, 0);
 
             }
