47ca10f882f1e113acf4343c8db21b42969fd656
ragavancse <ragavancse@gmail.com>
2023-03-04 13:53:23 +0530

    Log view Bug fix

--- SampleWinui/View/LogView.xaml.cs
+++ SampleWinui/View/LogView.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;  
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
@@ -30,10 +30,14 @@ namespace SampleWinui.View
                  new string("01/13/2015 01:59:01 678 PM: configuration saved by [DTear]"),
                  new string("01/13/2015 01:59:01 678 PM: configuration saved by [DTear]"),
                  new string("01/13/2015 01:59:01 678 PM: configuration saved by [DTear]"),
-                 
+
 
             };
-            LogViewCollection.ItemsSource= strings;
+            LogViewCollection.ItemsSource = strings;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LogViewCollection.ItemsSource = null;
         }
     }
 }
